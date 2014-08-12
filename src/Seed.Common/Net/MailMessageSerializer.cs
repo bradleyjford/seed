4f5ba0342887441d80411a66deb6a2a8b00db0ed
Bradley Ford <github@bradleyford.id.au>
2014-08-13 06:40:37 +1000

    Added missing MailMessageSerializer class

--- src/Seed.Common/Net/MailMessageSerializer.cs
+++ src/Seed.Common/Net/MailMessageSerializer.cs
@@ -3,57 +3,57 @@ using System.IO;
 using System.Net.Mail;
 using System.Reflection;
 
-namespace Track.Server.Mail
+namespace Seed.Common.Net
 {
-	public static class MailMessageSerializer
-	{
-		private static readonly ConstructorInfo MailWriterConstructor;
-		private static readonly MethodInfo SendMethod;
-		private static readonly MethodInfo CloseMethod;
+    public static class MailMessageSerializer
+    {
+        private static readonly ConstructorInfo MailWriterConstructor;
+        private static readonly MethodInfo SendMethod;
+        private static readonly MethodInfo CloseMethod;
 
-		static MailMessageSerializer()
-		{
-			var mailWriterType = typeof(MailMessage).Assembly.GetType("System.Net.Mail.MailWriter");
-			
-			MailWriterConstructor = mailWriterType.GetConstructor(
-				BindingFlags.Instance | BindingFlags.NonPublic,
-				null,
-				new [] { typeof(Stream) },
-				null);
+        static MailMessageSerializer()
+        {
+            var mailWriterType = typeof(MailMessage).Assembly.GetType("System.Net.Mail.MailWriter");
 
-			CloseMethod = mailWriterType.GetMethod(
-				"Close",
-				BindingFlags.Instance | BindingFlags.NonPublic);
+            MailWriterConstructor = mailWriterType.GetConstructor(
+                BindingFlags.Instance | BindingFlags.NonPublic,
+                null,
+                new[] { typeof(Stream) },
+                null);
 
-			SendMethod = typeof(MailMessage).GetMethod(
-				"Send",
-				BindingFlags.Instance | BindingFlags.NonPublic);
-		}
+            CloseMethod = mailWriterType.GetMethod(
+                "Close",
+                BindingFlags.Instance | BindingFlags.NonPublic);
 
-		public static string Serialize(MailMessage message)
-		{
-			string result;
+            SendMethod = typeof(MailMessage).GetMethod(
+                "Send",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+        }
 
-			using (var stream = new MemoryStream())
-			{
-				var mailWriter = MailWriterConstructor.Invoke(new object[] { stream });
+        public static string Serialize(MailMessage message)
+        {
+            string result;
 
-				SendMethod.Invoke(
-					message,
-					BindingFlags.Instance | BindingFlags.NonPublic,
-					null,
-					new [] { mailWriter, true, true },
-					null);
+            using (var stream = new MemoryStream())
+            {
+                var mailWriter = MailWriterConstructor.Invoke(new object[] { stream });
 
-				stream.Seek(0, SeekOrigin.Begin);
+                SendMethod.Invoke(
+                    message,
+                    BindingFlags.Instance | BindingFlags.NonPublic,
+                    null,
+                    new[] { mailWriter, true, true },
+                    null);
 
-				using (var reader = new StreamReader(stream))
-				{
-					result = reader.ReadToEnd();
-				}
-			}
+                stream.Seek(0, SeekOrigin.Begin);
 
-			return result;
-		}
-	}
+                using (var reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            return result;
+        }
+    }
 }
